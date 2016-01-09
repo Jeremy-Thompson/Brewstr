b393a6bdc1d864666822d19fd456fcdc1fd9afe8
Jeremy-Thompson <jdt4-@hotmail.com>
2016-01-08 16:39:32 -0800

    Setup Page Updates/Development

--- app/src/main/java/com/solutions/brewstr/Setup_Fragment.java
+++ app/src/main/java/com/solutions/brewstr/Setup_Fragment.java
@@ -3,23 +3,52 @@ package com.solutions.brewstr;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by jeremy on 2016-01-08.
  */
 public class Setup_Fragment extends Fragment implements View.OnClickListener {
     View rootview;
+
+    SeekBar mashTempSB;
+    SeekBar mashTimeSB;
+    SeekBar boilTempSB;
+    SeekBar boilTimeSB;
+    SeekBar hopsTimeSB;
+
+    TextView mashTempSP;
+    TextView MashTimeSP;
+    TextView boilTimeSP;
+    TextView boilTempSP;
+    TextView mashTimeSP;
+
+
     Button setupButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.setup_view,container,false);
+
+        //set button mapping and listeners
         setupButton = (Button) rootview.findViewById(R.id.setupButton);
         setupButton.setOnClickListener(this);
+
+        //set seekbar mapping and listeners
+        mashTempSB = (SeekBar) rootview.findViewById(R.id.mashingTempSeekBar);
+        mashTempSB.setOnSeekBarChangeListener(new seekBarListener());
+        mashTimeSB = (SeekBar) rootview.findViewById(R.id.mashingTimeSeekBar);
+        mashTimeSB.setOnSeekBarChangeListener(new seekBarListener());
+        boilTempSB = (SeekBar) rootview.findViewById(R.id.boilTempSeekBar);
+        boilTempSB.setOnSeekBarChangeListener(new seekBarListener());
+        boilTimeSB = (SeekBar) rootview.findViewById(R.id.boilTimeSeekBar);
+        boilTimeSB.setOnSeekBarChangeListener(new seekBarListener());
         return rootview;
     }
     @Override
@@ -28,4 +57,18 @@ public class Setup_Fragment extends Fragment implements View.OnClickListener {
         String tmp = "CLICK WORKED";
         setupButton.setText(tmp);
     }
+    private class seekBarListener implements SeekBar.OnSeekBarChangeListener {
+
+        public void onProgressChanged(SeekBar seekBar, int progress,
+        boolean fromUser) {
+            // Log the progress
+            Log.d("DEBUG", "Progress is: " + progress);
+            //set textView's text
+        }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+}
 }
