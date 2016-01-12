0408d29e0eebe194d4ee829c4dfa7905f3233cfd
Jeremy-Thompson <jdt4-@hotmail.com>
2016-01-11 16:31:40 -0800

    Updated Setup View
    Added seekbars with dynamically updating values. Added "generate udp
    message" algorithm.

--- app/src/main/java/com/solutions/brewstr/Setup_Fragment.java
+++ app/src/main/java/com/solutions/brewstr/Setup_Fragment.java
@@ -24,10 +24,11 @@ public class Setup_Fragment extends Fragment implements View.OnClickListener {
     SeekBar hopsTimeSB;
 
     TextView mashTempSP;
-    TextView MashTimeSP;
-    TextView boilTimeSP;
-    TextView boilTempSP;
     TextView mashTimeSP;
+    TextView boilTempSP;
+    TextView boilTimeSP;
+    TextView hopsTimeSP;
+
 
 
     Button setupButton;
@@ -42,27 +43,72 @@ public class Setup_Fragment extends Fragment implements View.OnClickListener {
 
         //set seekbar mapping and listeners
         mashTempSB = (SeekBar) rootview.findViewById(R.id.mashingTempSeekBar);
+        mashTempSB.setMax(25);
         mashTempSB.setOnSeekBarChangeListener(new seekBarListener());
         mashTimeSB = (SeekBar) rootview.findViewById(R.id.mashingTimeSeekBar);
+        mashTimeSB.setMax(50);
         mashTimeSB.setOnSeekBarChangeListener(new seekBarListener());
         boilTempSB = (SeekBar) rootview.findViewById(R.id.boilTempSeekBar);
         boilTempSB.setOnSeekBarChangeListener(new seekBarListener());
         boilTimeSB = (SeekBar) rootview.findViewById(R.id.boilTimeSeekBar);
         boilTimeSB.setOnSeekBarChangeListener(new seekBarListener());
+        hopsTimeSB = (SeekBar) rootview.findViewById(R.id.hopsTimeSeekBar);
+        hopsTimeSB.setOnSeekBarChangeListener(new seekBarListener());
+
+        //set the setpoint indicator mapping
+        mashTempSP = (TextView) rootview.findViewById(R.id.mashingTempSetpoint);
+        mashTimeSP = (TextView) rootview.findViewById(R.id.mashingTimeSetpoint);
+        boilTempSP = (TextView) rootview.findViewById(R.id.boilTempSetpoint);
+        boilTimeSP = (TextView) rootview.findViewById(R.id.boilTimeSetpoint);
+        hopsTimeSP = (TextView) rootview.findViewById(R.id.hopsTimeSetpoint);
         return rootview;
     }
     @Override
     public void onClick(View v) {
         //YOUR CODE HERE
-        String tmp = "CLICK WORKED";
-        setupButton.setText(tmp);
+        String messageToLL = "";
+        messageToLL += (75 + mashTempSB.getProgress());
+        messageToLL += ",";
+        messageToLL += (50 + mashTimeSB.getProgress());
+        messageToLL += ",";
+        messageToLL += boilTimeSB.getProgress();
+        setupButton.setText(messageToLL);
     }
     private class seekBarListener implements SeekBar.OnSeekBarChangeListener {
 
         public void onProgressChanged(SeekBar seekBar, int progress,
         boolean fromUser) {
             // Log the progress
-            Log.d("DEBUG", "Progress is: " + progress);
+            switch(seekBar.getId()){
+                case R.id.mashingTempSeekBar: {
+                    int setpoint = progress + 75;
+                    mashTempSP.setText("" + setpoint  + " (C)");
+                    break;
+                }
+                case R.id.mashingTimeSeekBar: {
+                    int setpoint = progress + 50;
+                    mashTimeSP.setText("" + setpoint + " (min)");
+                    break;
+                }
+                case R.id.boilTempSeekBar: {
+                    int setpoint = progress + 50;
+                    boilTempSP.setText("" + progress + " (C)");
+                    break;
+                }
+                case R.id.boilTimeSeekBar: {
+                    boilTimeSP.setText("" + progress + " (min)");
+                    break;
+                }
+                case R.id.hopsTimeSeekBar:{
+                    hopsTimeSP.setText("" + progress + " (min)");
+                }
+                default:
+                    break;
+            }
+
+
+
+            Log.i("DEBUG", "Progress is: " + progress);
             //set textView's text
         }
 
