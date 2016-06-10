d81b6436cc36f5406e2a924c5f9a9399ab75728f
Jeremy-Thompson <jdt4-@hotmail.com>
2016-06-10 14:47:47 -0700

    Brewstr Arduino Code
    Updated brew process - cleaned up timers and state machine software

--- Arduino/Brewstr/Brewstr.ino
+++ Arduino/Brewstr/Brewstr.ino
@@ -10,26 +10,24 @@
 // For Configuration Values: ALL_CAPS_WITH_UNDERSCORES
 
 // variables to store brew configuration/setpoints
-unsigned long e_mashing_temp_setpoint;
-unsigned long e_mashing_time_setpoint;
-
-
-unsigned long m_boiler_temp_setpoint = 0;
-unsigned long e_boiler_temp_setpoint;
-unsigned long e_boiler_time_setpoint;
-unsigned long e_hops_time_setpoint;
+unsigned long e_mashing_temp_setpoint = 2;
+unsigned long e_mashing_time_setpoint = 60;
+unsigned long m_kettle_temp_setpoint = 0;
+unsigned long e_boil_temp_setpoint = 0;
+unsigned long e_boil_time_setpoint = 60;
+unsigned long e_hops_time_setpoint = 60;
 
 // variables to store sensor/system states
-float m_boiler_temp_1 = 0;
-float m_boiler_temp_1_memory; //stores previous boiler temp value
+float m_kettle_temp_1 = 0;
+float m_kettle_temp_1_memory; //stores previous boiler temp value
 
 // variables to store sensor/system states
-float m_boiler_temp_2 = 0;
-float m_boiler_temp_2_memory; //stores previous boiler temp value
+float m_kettle_temp_2 = 0;
+float m_kettle_temp_2_memory; //stores previous boiler temp value
 
-int m_boiler_temp_avg_memory;
-int m_boiler_temp_avg = 0;
-float m_boiler_temp_avg_flt;
+int m_kettle_temp_avg_memory;
+int m_kettle_temp_avg = 0;
+float m_kettle_temp_avg_flt;
 float m_fermenterMass;
 float m_fermenterZeroMass;
 
@@ -59,9 +57,11 @@ boolean m_pump_bleed_off_valve = false;
 #define READ_TEMP_CYCLE 100U
 #define CONTROL_TEMP_CYCLE 500U
 #define CONTROL_SYSTEM_STATE_CYCLE 5000U
-#define CONTROL_PUMP_CYCLE 5000U
+#define CONTROL_PUMP_CYCLE 500U
+#define CONTROL_VALVES_CYCLE 500U
 #define MASH_OUT_OFFSET 45000U
 #define PUMP_PRIME_OFFSET 45000U
+#define MIN_TO_MILLI 60000U
 
 // Define the temperature sensor data pin = 3
 #define ONE_WIRE_BUS 3
@@ -74,17 +74,18 @@ unsigned long m_read_temp_last_millis = 0;
 unsigned long m_control_temp_last_millis = 0;
 unsigned long m_control_system_state_last_millis = 0;
 unsigned long m_control_pump_last_millis = 0;
+unsigned long m_control_valves_last_millis = 0;
 
 unsigned long m_start_brew_cycle_time;
 unsigned long m_elapsed_brew_cycle_time;
-unsigned long long m_time;
+unsigned long m_time;
 unsigned long m_time_remaining;
 
 
 String m_system_state = "Standby";
 
-  void setup()
-  {
+void setup()
+{
   Serial.begin(9600); //set baud rate for bluetooth serial input port
   // Start up the library
   sensors.begin();
@@ -92,23 +93,26 @@ String m_system_state = "Standby";
 
   //testing setup
   pinMode(HEATER_PIN, OUTPUT);
+  pinMode(DRAIN_VALVE_PIN,OUTPUT);
+  pinMode(LEVEL_VALVE_PIN,OUTPUT);
+  pinMode(PUMP_PIN,OUTPUT);
 }
 
 //----------------------------------------------
 // Main Running loop that calls the individual functions via task scheduler function (cycleCheck)
 //----------------------------------------------
-void loop() 
+void loop()
 {
   if(taskScheduler(&m_read_message_from_ul_last_millis, READ_MESSAGE_FROM_UL_CYCLE))  // if readpacket is currently scheduled to be called
   {
     //run task 1
     readMessageFromUL();
   }
-  if(taskScheduler(&m_read_temp_last_millis, READ_TEMP_CYCLE) && m_start_cfg_recvd) // if readtemp is currently scheduled to be called
+  if(taskScheduler(&m_read_temp_last_millis, READ_TEMP_CYCLE)) //&& m_start_cfg_recvd) // if readtemp is currently scheduled to be called
   {
     readTemp();
   }
-  if(taskScheduler(&m_control_temp_last_millis, CONTROL_TEMP_CYCLE) && m_start_cfg_recvd)    // task schedule template, add new tasks here and update names
+  if(taskScheduler(&m_control_temp_last_millis, CONTROL_TEMP_CYCLE)) //&& m_start_cfg_recvd)    // task schedule template, add new tasks here and update names
   {
     //run task  
     controlTemp();
@@ -120,6 +124,10 @@ void loop()
   if(taskScheduler(&m_control_pump_last_millis, CONTROL_PUMP_CYCLE))
   {
     controlPump();  
+  }
+  if(taskScheduler(&m_control_valves_last_millis, CONTROL_VALVES_CYCLE))
+  {
+    controlValves();
   }
 }
 
@@ -196,8 +204,8 @@ void decodeMessage(String message)
               //convert all strings into integers
               e_mashing_temp_setpoint = mash_temp.toInt();
               e_mashing_time_setpoint = mash_time.toInt();
-              e_boiler_temp_setpoint = boil_temp.toInt();
-              e_boiler_time_setpoint = boil_time.toInt();
+              e_boil_temp_setpoint = boil_temp.toInt();
+              e_boil_time_setpoint = boil_time.toInt();
               e_hops_time_setpoint = hops_time.toInt(); 
 
               if(!m_start_cfg_recvd)
@@ -223,23 +231,23 @@ void sendMessageToUL(String message)
 //---------------------------------------------------
 void readTemp()
 {
-   m_boiler_temp_1_memory = m_boiler_temp_1;
-   m_boiler_temp_2_memory = m_boiler_temp_2;
-   m_boiler_temp_avg_memory = m_boiler_temp_avg;
+   m_kettle_temp_1_memory = m_kettle_temp_1;
+   m_kettle_temp_2_memory = m_kettle_temp_2;
+   m_kettle_temp_avg_memory = m_kettle_temp_avg;
    sensors.requestTemperatures(); // Send the command to get temperatures
    
-   m_boiler_temp_1 = sensors.getTempCByIndex(0);
-   m_boiler_temp_2 = sensors.getTempCByIndex(1);
-   int boiler_temp_1_int = m_boiler_temp_1*100;
-   int boiler_temp_2_int = m_boiler_temp_2*100;
-   if(((m_boiler_temp_1 - m_boiler_temp_1_memory)!= 0)||((m_boiler_temp_2 - m_boiler_temp_2_memory)!= 0))
+   m_kettle_temp_1 = sensors.getTempCByIndex(0);
+   m_kettle_temp_2 = sensors.getTempCByIndex(1);
+   int boiler_temp_1_int = m_kettle_temp_1*100;
+   int boiler_temp_2_int = m_kettle_temp_2*100;
+   if(((m_kettle_temp_1 - m_kettle_temp_1_memory)!= 0)||((m_kettle_temp_2 - m_kettle_temp_2_memory)!= 0))
    {  
-      m_boiler_temp_avg = ( boiler_temp_1_int + boiler_temp_2_int)/2;
-      Serial.print("Temperature: ");
-      Serial.println(m_boiler_temp_avg);
-      Serial.print("Time: ");
-      Serial.println(millis() - m_start_brew_cycle_time);
-      Serial.println("----------------------------------------"); 
+      m_kettle_temp_avg = ( boiler_temp_1_int + boiler_temp_2_int)/2;
+      //Serial.print("Temperature: ");
+      //Serial.println(m_kettle_temp_avg);
+      //Serial.print("Time: ");
+      //Serial.println(millis() - m_start_brew_cycle_time);
+      //Serial.println("----------------------------------------"); 
 
    }
 }
@@ -262,13 +270,29 @@ boolean taskScheduler(unsigned long *lastMillis, unsigned int cycle)
 unsigned long controlSystemState()
 {
   unsigned long remaining_time;
+  
+  //calculate all the stage time setpoints
+  unsigned long mash_stage_time_setpoint = minToMilli(e_mashing_time_setpoint);
+  unsigned long mash_out_stage_time_setpoint = minToMilli(e_mashing_time_setpoint) + MASH_OUT_OFFSET;
+  unsigned long boil_prime_stage_time_setpoint = minToMilli(e_mashing_time_setpoint) + MASH_OUT_OFFSET + PUMP_PRIME_OFFSET;
+  unsigned long boil_stage_time_setpoint = minToMilli(e_mashing_time_setpoint) + MASH_OUT_OFFSET + PUMP_PRIME_OFFSET + minToMilli(e_boil_time_setpoint);
+  
   m_elapsed_brew_cycle_time = millis();
   m_time = m_elapsed_brew_cycle_time - m_start_brew_cycle_time;
+  
+  //Pre-heating stage
+  //-----------------------------------------------------------------
   if(!m_reached_mash_temp) 
   {
-    m_boiler_temp_setpoint = e_mashing_temp_setpoint;
+    //set valve and pump state
+    m_drain_valve = false;
+    m_level_valve = false;
+    m_pump_pwr = false;
+
+    //set other parameters
+    m_kettle_temp_setpoint = e_mashing_temp_setpoint;
     remaining_time = 0;
-    if(m_boiler_temp_avg/100 < (e_mashing_temp_setpoint - 2)){
+    if(m_kettle_temp_avg/100 < (e_mashing_temp_setpoint - 2)){
       m_system_state = "Pre-heating: ";
     }
     else{
@@ -276,51 +300,103 @@ unsigned long controlSystemState()
     m_reached_mash_temp = true;
     }
   }
-  else if(m_time <= e_mashing_time_setpoint*60000)
+  //Mashing Stage
+  //-----------------------------------------------------------------
+  else if(m_time <= mash_stage_time_setpoint)
   {
-    m_system_state = "Mashing";
-    m_boiler_temp_setpoint = e_mashing_temp_setpoint;
-    m_time_remaining = e_mashing_time_setpoint*60 - m_time/1000;
-  }
-  else if ((m_time > e_mashing_time_setpoint*60000) && (m_time < (e_mashing_time_setpoint*60000 + e_boiler_time_setpoint*60000)))
-  {
-    if(m_time < (e_mashing_time_setpoint*60000 + MASH_OUT_OFFSET))
-    {
-      m_system_state = "Mash Out";
-      m_boiler_temp_setpoint = e_boiler_temp_setpoint;
-      m_time_remaining = (e_mashing_time_setpoint*60 - MASH_OUT_OFFSET/1000) - m_time/1000;
-    }
-    else{
-      m_drain_valve = true;
-      m_level_valve = true;
-      if(m_system_state == "Mash Out")
-      {
-        primePump();
-      }
-      m_system_state = "Boiling";
-      m_boiler_temp_setpoint = e_boiler_temp_setpoint;
-      m_time_remaining = (e_boiler_time_setpoint + e_mashing_time_setpoint)*60 - m_time/1000;
-      m_pump_pwr = true;
-    }
-  }
-  else {
+    //set valve and pump state
+    m_drain_valve = false;
     m_level_valve = false;
+    m_pump_pwr = false;
+
+    //set other parameters
+    m_system_state = "Mashing";
+    m_kettle_temp_setpoint = e_mashing_temp_setpoint;
+    m_time_remaining = milliToMin(mash_stage_time_setpoint - m_time);
+  }
+  //Mash Out Stage
+  //----------------------------------------------------------------
+  else if (m_time < mash_out_stage_time_setpoint)
+  { 
+    //set valve and pump state
+    m_drain_valve = false;
+    m_level_valve = false;
+    m_pump_pwr = false;
+    
+    //set other parameters
+    m_system_state = "Mash Out";
+    m_kettle_temp_setpoint = e_boil_temp_setpoint;
+    m_time_remaining = milliToMin(mash_out_stage_time_setpoint - m_time);
+   }
+  //Boiling/Priming stage
+  //--------------------------------------------------------------
+  else if (m_time < boil_prime_stage_time_setpoint){
+    //set valve and pump state
+    if( m_time < (mash_out_stage_time_setpoint + PUMP_PRIME_OFFSET))
+    {
+      m_drain_valve = true;
+    }else{
+      m_drain_valve = false;
+    }
+
+    m_level_valve = true;
+    m_pump_pwr = false;
+    //set other parameters
+    m_system_state = "Boiling/Priming";
+    m_kettle_temp_setpoint = e_boil_temp_setpoint;
+    m_time_remaining = milliToMin(boil_prime_stage_time_setpoint - m_time);
+
+  }
+  //Boiling stage
+  //-------------------------------------------------------------
+  else if (m_time < boil_stage_time_setpoint)
+  {
+    //set valve and pump state
+    m_drain_valve = true;
+    m_level_valve = true;
+    m_pump_pwr = true;
+    
+    //set other parameters
+    m_system_state = "Boiling";
+    m_kettle_temp_setpoint = e_boil_temp_setpoint;
+    m_time_remaining = milliToMin(boil_stage_time_setpoint - m_time);
+  }
+  //Finished stage
+  //-------------------------------------------------------------
+  else
+  {
+    //set valve and pump state
+    m_level_valve = false;
+    m_drain_valve = true;
+    m_pump_pwr = false;
+    
+    //set other parameters
     m_system_state = "Finished";
-    m_boiler_temp_setpoint = 0;
+    m_kettle_temp_setpoint = 0;
     remaining_time = 0;
+    m_time_remaining = 0;
   }
   Serial.print("System State: ");
   Serial.println(m_system_state);
   Serial.print("Time Remaining: ");
   Serial.print(m_time_remaining);
-  Serial.println(" seconds");
+  Serial.println(" minutes");
   Serial.print("Boiler Temperature Setpoint: ");
-  Serial.println(m_boiler_temp_setpoint);
+  Serial.println(m_kettle_temp_setpoint);
   Serial.print("Boiler Temperature Feedback: ");
-  Serial.println(m_boiler_temp_avg_flt);
+  Serial.println(m_kettle_temp_avg_flt);
   Serial.print("Heater PWM Signal: ");
-  Serial.print(m_pwm_output*100/255);
-  Serial.println(" %");
+  Serial.println(m_pwm_output*100/255);
+  Serial.print("Pump Signal: ");
+  Serial.println(m_pump_pwr);
+  Serial.print("Drain Valve Signal: ");
+  Serial.println(m_drain_valve);
+  Serial.print("Level Valve Signal: ");
+  Serial.println(m_level_valve);
+  Serial.print("Temp Sensor 1: ");
+  Serial.println(m_kettle_temp_1);
+  Serial.print("Temp Sensor 2 : ");
+  Serial.println(m_kettle_temp_2);
   Serial.println();
   Serial.println("----------------------------------------");
   Serial.println();
@@ -330,11 +406,11 @@ unsigned long controlSystemState()
 //--------------------------------------------------
 void controlTemp()
 {
-  m_boiler_temp_avg_flt = m_boiler_temp_avg;
-  m_boiler_temp_avg_flt = m_boiler_temp_avg_flt/100;
-  float temp_difference= (m_boiler_temp_setpoint - m_boiler_temp_avg_flt);
-  float output = temp_difference*75 + (m_boiler_temp_avg - m_boiler_temp_avg_memory)*1;
-  float boiler_temp_avg_float = m_boiler_temp_avg;
+  m_kettle_temp_avg_flt = m_kettle_temp_avg;
+  m_kettle_temp_avg_flt = m_kettle_temp_avg_flt/100;
+  float temp_difference= (m_kettle_temp_setpoint - m_kettle_temp_avg_flt);
+  float output = temp_difference*75 + (m_kettle_temp_avg - m_kettle_temp_avg_memory)*1;
+  float boiler_temp_avg_float = m_kettle_temp_avg;
   boiler_temp_avg_float = boiler_temp_avg_float/100;
   if( output > 100)
   {
@@ -399,6 +475,10 @@ void controlPump()
   {
     analogWrite(PUMP_PIN,OPEN);
   }
+  else
+  {
+    analogWrite(PUMP_PIN,CLOSE);
+  }
 }
 void primePump()
 {
@@ -412,24 +492,29 @@ void primePump()
   }
   m_pump_bleed_off_valve = false;
 }
-void controlValveState()
+void controlValves()
 {
   if(m_drain_valve)
   {
-    valveAction("drain",OPEN);
+    analogWrite(DRAIN_VALVE_PIN, OPEN); 
   }else
   {
-    valveAction("drain",CLOSE);
+    analogWrite(DRAIN_VALVE_PIN, CLOSE); 
   }
   if(m_level_valve)
   {
-    valveAction("level",OPEN);
+    analogWrite(LEVEL_VALVE_PIN, OPEN); 
   }else
   {
-    valveAction("level",CLOSE);
+    analogWrite(LEVEL_VALVE_PIN, CLOSE); 
   }    
 }
-
-
-
+unsigned long minToMilli(unsigned long val)
+{
+  return val*MIN_TO_MILLI;
+}
+unsigned long milliToMin(unsigned long val)
+{
+  return val/MIN_TO_MILLI;
+}
 
